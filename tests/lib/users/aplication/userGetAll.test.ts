539a1f59f068cfde4e514d53bb4d9d93f094f467
LeanCG <leandrocorrea040@gmail.com>
2024-09-20 22:51:43 -0300

    first steps into test for hexagonal architecture

---
+++ tests/lib/users/aplication/userGetAll.test.ts
@@ -0,0 +1,8 @@
+import { UserGetAll } from "src/lib/users/aplication/User/UserGetAll";
+
+describe("UserGetAll Should", ()=>{
+    test("Return All Users", async ()=>{
+        const UserCase = new UserGetAll();
+
+    });
+})
